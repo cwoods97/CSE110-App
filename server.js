dfefac112d0e441f0dda49f9a7199d588b9b09f6
John Yoon <j.elijah.yoon@gmail.com>
2017-11-10 09:01:02 -0800

    Removed dependency on 'build' directory for when express is used in non-production

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const express = require('express')
 const path = require('path');
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'client/build')))
+/*  
+ *  Serves static files from the 'client/build' directory
+ *  Navigating to root of webserver serves 'index.html'
+ *  Disable this functionality when not in production (running two separate servers)
+ */
+var is_production = process.argv[2]
+if (is_production) {
+	app.use(express.static(path.join(__dirname, 'client/build')));
+}
 
 app.get('/api/hello', function(req, res){
 	res.json({message: "Hello World - Express API is live"});
