1a7b28caa133bacdbac27e4183316faaf8bf014a
Max Zhao <zhao.max7379@gmail.com>
2017-11-10 15:47:13 -0800

    basic routing setup

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const admin = require('firebase-admin');
 const path = require('path');
+const hello = require('./routes/hello');
+const create_account = require('./routes/create_account')
+
 const app = express();
+
+app.use('/api/hello', hello);
+app.use('/api/create_account', create_account);
 
 // Check whether object has any members
 var is_empty = (obj) => { return Object.keys(obj).length };
@@ -9,7 +15,6 @@ var is_empty = (obj) => { return Object.keys(obj).length };
 // Initialize connection to firebase
 // https://firebase.google.com/docs/admin/setup
 var serviceAccount = require("./firebase_sac.json");
-
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://speakeasy-25a66.firebaseio.com"
@@ -20,29 +25,6 @@ var is_production = process.argv[2]
 if (is_production) {
 	app.use(express.static(path.join(__dirname, 'client/build')));
 }
-
-// TODO Start moving routes into a 'routes' folder and use express router
-// TODO Put in the actual route names (see design use cases)
-app.get('/api/hello', (req, res) => {
-	res.json({message: "Hello World - Express API is live"});
-})
-
-app.get('/register', (req, res) => {
-	var q_name = res.query.displayName;
-	var q_email = res.query.email;
-	var q_passwd = res.query.passwd;
-
-	if (q_displayName && q_email && q_passwd) {
-		// Firebase logs a user in when account is created
-		// https://firebase.google.com/docs/reference/js/firebase.User
-		admin.auth().createUser({
-			displayName: q_name,
-			email: q_email,
-			password: q_password
-		// TODO Write to a physical debug log instead of using console (output won't be captured)
-		}).catch((err) => {console.log(err);})
-	}
-})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function() {
