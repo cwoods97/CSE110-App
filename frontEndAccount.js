ce336105c044043f267e66109f6f4caf38793793
John Yoon <j.elijah.yoon@gmail.com>
2017-11-12 06:41:07 -0800

    Rewrote code from a front-end perspective, not dependent on node.js. Implemented skeleton code structure for account creation and login, indicating which sections need to be further hashed out.

--- frontEndAccount.js
+++ frontEndAccount.js
@@ -1,62 +1,83 @@
-const http = require('http')
-const firebase = require('firebase')
 
-//frontend doesn't do app.get I believe, but the methods should look something like this (this is just a start)
-app.get('/api/create_account', (req, res) => {
-	res.json({message: "'create_account api' responding"});
-	var q_name = res.query.displayName;
-	var q_email = res.query.email;
-	var q_passwd = res.query.passwd;
+// Making HTTP requests in React: https://github.com/github/fetch
+// The frontend shall create a user account through completing two separate objectives:
+//     1. Creating a user account on the Firebase database (POST Display name, Email, Password)
+//     2. Providing the user id (returned by Firebase) to the backend (to create a parallel user account in the backend)
+if (displayName && email && passwd) {
+	// *** STEP 1 ***
+	// Firebase logs a user in when account is created (Consider logging user out immediately since frontend redirects user to login page upon account creation)
+	// https://firebase.google.com/docs/reference/js/firebase.User
+    // https://firebase.google.com/docs/reference/js/firebase.auth.Auth#createUserWithEmailAndPassword
+    var firebaseUser;
 
-	if (q_displayName && q_email && q_passwd) {
-		// Firebase logs a user in when account is created
-		// https://firebase.google.com/docs/reference/js/firebase.User
-                // Credits: https://firebase.google.com/docs/reference/js/firebase.auth.Auth
-		var user = firebase.auth().createUserWithEmailAndPassword(q_email, q_password).catch(function(error)) {
+	firebase.auth().createUserWithEmailAndPassword(email, password)
+		.then(user => {
+			firebaseUser = user;
+
+			// TODO: IF display name is also unique, we need to iterate over all users to verify it's unique
+			// https://firebase.google.com/docs/reference/js/firebase.User#updateProfile
+			user.updateProfile({ displayName: displayName })
+				.catch(error => {
+					// TODO: Write code to handle this edge case - Firebase unable to update display name
+					// Either try to update again or just display an error in the front end
+				});
+		}).catch(error => {
 			var errorCode = error.code;
 			var errorMessage = error.message;
-			if (errorCode === 'auth/weak-password') {
-				alert('The password is too weak.');
-			} else {
-				alert(errorMessage);
- 			}
-  			console.log(error);
+			switch (errorCode) {
+				case 'auth/email-already-in-use':
+					// TODO: Write code to prompt user for different email address
+					break;
+				case 'auth/weak-password':
+					// TODO: Passwords must be at least 6 characters.
+					break;
+				case 'auth/invalid-email':
+					// Should already be handled by frontend input validation
+					break;
+				case 'auth/operation-not-allowed':
+					// Should not be a problem - account creation is enabled in Firebase.
+					break;
+
+			}
+
+			return;
 		});
-		user.updateProfile(displayName: q_name);
 
-		var post_data = querystring.stringify({
-      			'token': user.getToken(true),
-		});		
+	// *** STEP 2 ***
+	var userData = new FormData();
+	userData.append('uid', user.uid);
+	userData.append('token', user.getIdToken());
 
-		var post_options = {
-	    		host: '',//insert url here
-    	  		port: '3000',//insert port here
-    	  		path: '/api/register',
-	      		method: 'POST',
-	      		headers: {
-	        	  	'Content-Length': Buffer.byteLength(post_data)
-	      		}
-		};
+	fetch('/api/register', {
+		method: 'POST',
+		body: userData,
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	}).catch(error => {
+		// TODO: Could not create a backend account. Try again maybe?
+	})
+}
 
-		var post_req = http.request(post_options);
-		post_req.write(post_data);
-		post_req.end();
-	}
-})
-app.get('/api/login', (req, res) => {
+// =============================================================================================================
+// TODO: Write out code for logging in to Firebase
+// There's a method user.getIdToken. Store it on the client's computer and send it every time you call something in the backend
+// 
+if (email && passwd) {
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    // Credits: https://firebase.google.com/docs/reference/js/firebase.auth.Auth
+    firebase.auth().signInWithEmailAndPassword(email, passwd)
+    	.then(user => {
+    		var token = user.getIdToken();
+    		// TODO: Store it on the client side - must be used for every request to backend
 
-        if (q_email && q_passwd) {
-                // https://firebase.google.com/docs/reference/js/firebase.User
-                // Credits: https://firebase.google.com/docs/reference/js/firebase.auth.Auth
-                var user = firebase.auth().signInWithEmailAndPassword(q_email, q_password).catch(function(error) {
-                        var errorCode = error.code;
-                        if(errorCode === 'auth/wrong-password') {
-                                alert('wrong password');
-                        } else {
-                                alert(errorMessage);
-                        }
-                        console.log(error);
-                });
-                // There's a method user.getToken(true). Store it on the client's computer and send it every time you call something in the backend
-        }
-}*
+    	}).catch(error => {
+    		// TODO: Fill out the rest of this code
+		    var errorCode = error.code;
+		    if(errorCode === 'auth/wrong-password') {
+		            alert('wrong password');
+		    } else {
+		            alert(errorMessage);
+		    }
+		});
+}
