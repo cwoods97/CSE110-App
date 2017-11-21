16d08277c4e9567117f8c7ff78ed4fa5aa9cb445
John Elijah Yoon <j.elijah.yoon@gmail.com>
2017-11-20 19:03:14 -0800

    Refactored backend code, enforcing 4 space code standard and updating logic for referencing admin & token

--- routes/session.js
+++ routes/session.js
@@ -2,27 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/join', (req, res) => {
+		var user = req.locals.uid;
+		var session = req.accessCode; //assuming this is how it's stored
 
-	var user = req.verifyIdToken().uid;
-	var session = req.accessCode; //assuming this is how it's stored
+		//get database reference
+		var db = req.locals.admin.database();
+		var ref = db.ref("sessions"); //assuming sessions are stored here
 
-	//get database reference
-	var db = req.locals.admin.database();
-	var ref = db.ref("sessions"); //assuming sessions are stored here
-	
-	//find session in database
-	ref.orderByKey().equalTo(session);
-	//add user to list of participants
-	updateString = "/participants/" + user;
-	ref.update({updateString: true});
+		//find session in database
+		ref.orderByKey().equalTo(session);
+		//add user to list of participants
+		updateString = "/participants/" + user;
+		ref.update({updateString: true});
 
-	ref = db.ref("user"); //assuming users are stored here
+		ref = db.ref("user"); //assuming users are stored here
 
-	//find user in database
-	ref.orderByKey().equalTo(user);
-	//add session to joinedSessions - update
-	updateString = '/joinedSessions/' + session;
-	ref.update({updateString: session});
+		//find user in database
+		ref.orderByKey().equalTo(user);
+		//add session to joinedSessions - update
+		updateString = '/joinedSessions/' + session;
+		ref.update({updateString: session});
 
 });
 
--- routes/sessionSetup.js
+++ routes/sessionSetup.js
@@ -7,32 +7,29 @@ var db = admin.database();
 var sessionRef = db.ref("session");
 
 router.get('/create_session', (req, res) => {
-	//token should have been verified, and this should be the decodedToken
-        var token = res.query.token;
-        if (!token) {
-		//error, there should not be a null token
-	}
+    //token should have been verified, and this should be the decodedToken
+    var token = res.query.token;
 
-	var uid = token.uid;
-	var accessCode = 0;
-	var uniqueAccessCode = false;
-	var max = 500000;
-	var min = 100000;
-	while(!uniqueAccessCode) {
-		accessCode = Math.floor(Math.random()*(max - min))+min;
-		//query database for active session with accessCode. If not found, uniqueAccessCode = true
-	}
-	//create database object with default values and uid
-	sessionRef.set({
+  	var uid = token.uid;
+  	var accessCode = 0;
+  	var uniqueAccessCode = false;
+  	var max = 500000;
+  	var min = 100000;
+  	while(!uniqueAccessCode) {
+  		  accessCode = Math.floor(Math.random()*(max - min))+min;
+        //query database for active session with accessCode. If not found, uniqueAccessCode = true
+  	}
+  	//create database object with default values and uid
+  	sessionRef.set({
 
 
-	
-	
-	//query database for user object with matching uid
-	//add the session ID to the list of presented sessions in the user object
-	
- 	res.writeHead(200, {'Content-Type': 'application/json'})
-	res.end(JSON.stringify(result));
+
+
+  	//query database for user object with matching uid
+  	//add the session ID to the list of presented sessions in the user object
+
+   	res.writeHead(200, {'Content-Type': 'application/json'})
+  	res.end(JSON.stringify(result));
 })
 
         constructor(props){
@@ -62,4 +59,3 @@ router.get('/create_session', (req, res) => {
                 });
                 this.setState({name: '', username: '', password: ''});
         }
-
