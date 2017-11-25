ccfd7f56c9aeddfad716c770c864763e059ad83f
mharastiCSE30 <mharasti@ucsd.edu>
2017-11-25 13:08:27 -0800

    Fixed First Page back to App.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import './styles/index.css';
-import App from './Join';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
