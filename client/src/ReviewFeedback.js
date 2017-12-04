058c404217bc241889d50e09fe165536f64645d3
mharastiCSE30 <mharasti@ucsd.edu>
2017-12-03 20:16:52 -0800

    Started Audio styling

--- client/src/ReviewFeedback.js
+++ client/src/ReviewFeedback.js
@@ -131,7 +131,7 @@ class ReviewFeedback extends Component {
                             id="audio"
                             controls={true}
                             src={this.state.src}
-                            style={{width:'98%',float:'both',marginLeft:'2px',backgroundColor:'#333333'}}
+                            style={{width:'98%',float:'both',marginLeft:'2px'}}
                         />
                     </div>
 
